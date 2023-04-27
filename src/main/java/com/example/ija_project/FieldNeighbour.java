0b5ed67145038311c4fb0c36a9e3015dc402029c
xcechm13 <roharope@seznam.cz>
2023-04-27 11:54:08 +0200

    creating a map based on text input

---
+++ src/main/java/com/example/ija_project/FieldNeighbour.java
@@ -0,0 +1,20 @@
+package com.example.ija_project;
+
+public enum FieldNeighbour {
+    NoNeighbour,
+    RightNeighbour,
+    LeftNeighbour,
+    LeftRightNeighbour,
+    DownNeighbour,
+    DownRightNeighbour,
+    DownLeftNeighbour,
+    DownLeftRightNeighbour,
+    UpNeighbour,
+    UpRightNeighbour,
+    UpLeftNeighbour,
+    UpLeftRightNeighbour,
+    UpDownNeighbour,
+    UpDownRightNeighbour,
+    UpDownLeftNeighbour,
+    UpDownLeftRightNeighbour,
+}
---
+++ src/main/java/com/example/ija_project/FieldPixels.java
@@ -0,0 +1,2 @@
+package com.example.ija_project;public class FieldPixels {
+}
